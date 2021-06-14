ca6d1cd68894192c32382714b11864c926f3e368
njmbb8 <nmcgr001@gmail.com>
2021-06-14 19:16:44 -0400

    commit tree 4677271a6e7eae5a9cceec446d6b8823b15af457

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function superbowlWin(winRecord){
+    let ret = winRecord.find(function(record){
+        let ret;
+        if(record.result === "W"){
+            return true;
+        }
+        else{
+            return false;
+        } 
+    });
 
+    if(ret !== undefined){
+        return ret.year;
+    }
+    else{
+        return undefined;
+    }
+}
